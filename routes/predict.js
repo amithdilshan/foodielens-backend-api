07fd67319ace5bbc7aacfd6bba70fd740d5e82d9
amithdilshan <amithdilshan89@gmail.com>
2025-05-15 14:02:01 +0900

    Delete routes directory

--- routes/predict.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const { predict } = require('../controllers/predictController');
-
-const router = express.Router();
-
-router.post('/', predict);
-
-module.exports = router;
